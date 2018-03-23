2552e2f50a54febdc0ab1d4c43d69dee6512938e
j-runser <j-runser@onu.edu>
2018-03-23 19:38:30 +0000

    Finished working copy of exercise3.cpp, I plan on compleating the exercise in a diffrent way as well.

--- exercise3.cpp
+++ exercise3.cpp
@@ -2,7 +2,7 @@
  * 
  * Author: Joe Runser
  * Created: 23 February 2018
- * Updated: 23 February 2018
+ * Updated: 21 March 2018
  *
  * Pass commands to the operating system. The begginings of a shell.
  */
@@ -11,47 +11,78 @@
 #include <string>
 #include <sstream>
 #include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
 extern char **environ;
 
+const int MAX_BUFF = 1024;
+
 int main(int argc, char *argv[]) {
 
     string line;
-    cout << "=> ";
-    
+    string cmdFirst;
+    string cmdNext;
+
+    string dirCurr = getcwd(NULL, MAX_BUFF);
+       
+    cout << dirCurr << " => ";
+
     while(getline(cin, line)) {
 
-        string cmdFirst;
         istringstream strm;
 
         strm.str(line);
         strm >> cmdFirst;
+        strm >> cmdNext;
 
-        if(line == "dir") {
+        if(cmdFirst == "dir") {
             line.replace(0, 3, "ls -al");
             cout << line << endl;
             system(line.c_str());
         }
-        else if(line == "clear" || line == "clc") {
+        else if(cmdFirst == "clear" || cmdFirst == "clc") {
             system("clear");
         }
-        else if(line == "environ") {
+        else if(cmdFirst == "cd") {
+            // Set the current running directory.
+            if(chdir(cmdNext.c_str()) != 0) {
+                cout << "Error: Could not change the directory." << endl;
+            }
+
+            string dirTmp = getcwd(NULL, MAX_BUFF);
+            // Set the environment variable.
+            if(setenv("PWD", dirTmp.c_str(), 1) != 0) {
+                cout << "Error: Could not set the environment variable 'PWD'";
+                cout << dirCurr << endl;
+                
+                // If the working directory cannot be changed back to previous
+                // working directory. It will return 1 as a fatal error.
+                if(chdir(dirCurr.c_str()) != 0) {
+                    cout << "Error: Fatal error with cd command." << endl;
+                    return 1;
+                }
+            }
+            // Unlike in the in the eample, I am using dirCurr to hold the
+            // output that will be going to the command line so I will not
+            // to clear up memory.
+            dirCurr = getcwd(NULL, MAX_BUFF);
+        }
+        else if(cmdFirst == "environ") {
             for(int i = 0; environ[i] != NULL; i++) {
                 cout << environ[i] << endl;
             }
         }
-        else if(line == "quit" || line == "exit" || line == "Q" || line == "q") {
+        else if(cmdFirst == "quit" || cmdFirst == "exit" || cmdFirst == "Q" || cmdFirst == "q") {
             break;
         }
         else {
             system(line.c_str());
         }
 
-        cout << "=> ";
-    }
+        cout << dirCurr << " => ";
+    } // while end
 
     return 0;
-
 }
